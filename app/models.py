341eb5cef763cdb99c3e8c9989a14e0e72eba320
magechaP <magechapeter@gmail.com>
2019-05-30 16:07:35 +0300

    add columns to database

--- app/models.py
+++ app/models.py
@@ -13,10 +13,22 @@ def load_user(user_id):
     '''
     return User.query.get(int(user_id))
 
-class User(db.Model):
+class User(UserMixin,db.Model):
+    """ class modelling the users """
+
     __tablename__='users'
+
+    #create the columns
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True, index =True)
+    password_hash = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pitches = db.relationship("Pitch", backref="user", lazy = "dynamic")
+    comment = db.relationship("Comments", backref="user", lazy = "dynamic")
+    vote = db.relationship("Votes", backref="user", lazy = "dynamic")
 
 
     def __repr__(self):
