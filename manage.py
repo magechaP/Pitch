0aee28c8e92ad676e9f8ef6f04d99f0e011ecaa0
magechaP <magechapeter@gmail.com>
2019-05-30 20:02:30 +0300

    create an app instance

--- manage.py
+++ manage.py
@@ -3,3 +3,13 @@ from flask_script import Manager, Server
 from app.models import User,Pitch,Comments,PitchCategory
 
 from flask_migrate import Migrate, MigrateCommand
+
+# Creating app instance
+app = create_app('development')
+app = create_app('production')
+
+manager = Manager(app)
+manager.add_command('server', Server)
+
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
