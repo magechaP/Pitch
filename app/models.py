1959cf1cd638a8595235ae1b54dfb16fcc96e806
magechaP <magechapeter@gmail.com>
2019-05-30 16:08:59 +0300

    implement the securing passwords functionality

--- app/models.py
+++ app/models.py
@@ -31,5 +31,17 @@ class User(UserMixin,db.Model):
     vote = db.relationship("Votes", backref="user", lazy = "dynamic")
 
 
+    # securing passwords
+    @property
+    def password(self):
+        raise AttributeError('You can not read the password Attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
     def __repr__(self):
         return f'User {self.username}'
