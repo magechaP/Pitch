93a3ce4212dc49a7e9a6a5002a65f48d5245b80b
magechaP <magechapeter@gmail.com>
2019-05-30 20:10:38 +0300

    create exports in shell start file

---
+++ start.sh
@@ -0,0 +1,4 @@
+export SECRET_KEY='yang lanti'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://petermagecha:petermagecha@localhost/pitch'
+export MAIL_USERNAME='magechapeter@gmail.com'
+export MAIL_PASSWORD='yangvigilanti'
