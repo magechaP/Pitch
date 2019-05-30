561a2c13a058544d59ed940454dd0063d29df2ce
magechaP <magechapeter@gmail.com>
2019-05-30 20:04:13 +0300

    create a command decorator to run unit tests

--- manage.py
+++ manage.py
@@ -13,3 +13,10 @@ manager.add_command('server', Server)
 
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
+
+@manager.command
+def test():
+    """Run unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
