5dbcb5981d3733754ecc02ca6247299ea890be3b
magechaP <magechapeter@gmail.com>
2019-05-31 08:56:25 +0300

    add  more configuration functionality

--- config.py
+++ config.py
@@ -1,4 +1,17 @@
+import os
+
 class Config:
+    '''Main configuration class'''
+
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql + psycopg2://petermagecha:password@localhost/pitch'
 
+    SECRET_KEY = "yang lanti"
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     
