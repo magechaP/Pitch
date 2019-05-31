2400f09b2e92b304d2ef804a6ae11782be0a90cf
magechaP <magechapeter@gmail.com>
2019-05-31 08:57:20 +0300

    add code to enable start of file

--- start.sh
+++ start.sh
@@ -2,3 +2,5 @@ export SECRET_KEY='yang lanti'
 export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://petermagecha:petermagecha@localhost/pitch'
 export MAIL_USERNAME='magechapeter@gmail.com'
 export MAIL_PASSWORD='yangvigilanti'
+
+python3.6 manage.py server
